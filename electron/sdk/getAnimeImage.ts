8016e67739218e47bcbfc53b18272e23b781725b
jeweells <37516710+jeweells@users.noreply.github.com>
2021-06-18 17:37:28 -0400

    Loads a better quality image in animepeek

---
+++ electron/sdk/getAnimeImage.ts
@@ -0,0 +1,12 @@
+import { malImageUrlToImage } from './getAnimeRecommendations'
+import { searchMalAnime } from './searchAnime'
+
+export const getAnimeImage = (animeName: string) => {
+    return getMalAnimeImage(animeName)
+}
+
+export const getMalAnimeImage = async (animeName: string) => {
+    const animes = await searchMalAnime(animeName)
+    // eslint-disable-next-line camelcase
+    return malImageUrlToImage(animes?.[0]?.image_url)
+}
