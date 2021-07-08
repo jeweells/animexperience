162af032d8a8afdf2a0291275c10095cad2ee0a2
jeweells <abraham.pacheco6319@gmail.com>
2021-07-08 16:20:19 -0400

    Close button

--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -1,3 +1,4 @@
+import { ipcRenderer } from 'electron'
 import React, {
     createContext,
     Fragment,
@@ -7,10 +8,12 @@ import React, {
     useState,
 } from 'react'
 import { CSSTransition, SwitchTransition } from 'react-transition-group'
+import { Icon, IconButton } from 'rsuite'
 import styled from 'styled-components'
 import { useAppSelector } from '../../../redux/store'
 import SearchBar from '../SearchBar'
 import fade from './fade.module.less'
+
 const Wrapper = styled.div`
     background: #25282e;
     position: fixed;
@@ -20,6 +23,8 @@ const Wrapper = styled.div`
     z-index: 100000;
     -webkit-user-select: none;
     -webkit-app-region: drag;
+    display: flex;
+    align-items: center;
 `
 
 const Container = styled.div`
@@ -29,6 +34,14 @@ const Container = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    height: 100%;
+    flex: 1;
+`
+const Buttons = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    padding-right: 16px;
     height: 100%;
 `
 
@@ -43,6 +56,10 @@ export type TopbarProps = {}
 
 export const ContentContext = createContext<React.RefObject<HTMLDivElement> | null>(null)
 export const useContentRef = () => useContext(ContentContext)
+
+const closeApp = () => {
+    return ipcRenderer.invoke('closeApp')
+}
 
 export const Topbar: React.FC<TopbarProps> = React.memo(({ children }) => {
     const watching = useAppSelector((d) => d.watch.watching)
@@ -83,6 +100,17 @@ export const Topbar: React.FC<TopbarProps> = React.memo(({ children }) => {
                             </Container>
                         </CSSTransition>
                     </SwitchTransition>
+                    <Buttons>
+                        <IconButton
+                            style={{
+                                // @ts-ignore
+                                WebkitAppRegion: 'no-drag',
+                                WebkitUserSelect: 'all',
+                            }}
+                            onClick={closeApp}
+                            icon={<Icon icon={'close'} />}
+                        />
+                    </Buttons>
                 </Wrapper>
             )}
             <Content
