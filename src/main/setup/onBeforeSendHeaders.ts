70c51829bc056987bbdd2269092357ea03e14faa
jeweells <abraham.pacheco6319@gmail.com>
2024-02-04 15:01:08 +0100

    chore: removed onBeforeSendHeaders.ts logs which spams the text so much

--- src/main/setup/onBeforeSendHeaders.ts
+++ src/main/setup/onBeforeSendHeaders.ts
@@ -1,11 +1,8 @@
 import { session } from 'electron'
 import { PUBLIC_PATH } from '../constants'
-import { debug } from '@dev'
 
 export const onBeforeSendHeaders = () => {
   session.defaultSession.webRequest.onBeforeSendHeaders(async (details, callback) => {
-    debug('onBeforeHeaders', details.url)
-
     if (
       ['https://jkanime.net/', 'https://www3.animeflv.net/', 'https://animeflv.net/'].some((u) =>
         details.url.startsWith(u)
@@ -19,7 +16,6 @@ export const onBeforeSendHeaders = () => {
         details.requestHeaders.Referer.includes(PUBLIC_PATH)
       ) {
         details.requestHeaders.Referer = details.url
-        debug('ADDED REFERER FOR', details.url)
       }
     }
     return callback({ cancel: false, requestHeaders: details.requestHeaders })
