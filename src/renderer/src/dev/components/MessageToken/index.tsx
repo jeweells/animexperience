08cba04e91d21f686a2d146131665313db7790b5
jeweells <abraham.pacheco6319@gmail.com>
2024-02-04 15:40:29 +0100

    fix: in operator should be made on objects

--- src/renderer/src/dev/components/MessageToken/index.tsx
+++ src/renderer/src/dev/components/MessageToken/index.tsx
@@ -7,6 +7,6 @@ import { TraceToken } from './TraceToken'
 export const MessageToken = ({ token }: { token: ForcedAny }) => {
   if (typeof token === 'string') return <TextToken token={token} />
   if (typeof token === 'number') return <NumberToken token={token} />
-  if ('$stack' in token) return <TraceToken token={token} />
+  if (typeof token === 'object' && '$stack' in token) return <TraceToken token={token} />
   return <ObjectToken token={token} />
 }
