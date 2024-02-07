dad622edfd203f76d1a6aba4e025061157f308dd
jeweells <abraham.pacheco6319@gmail.com>
2024-02-07 21:14:24 +0100

    fix: fails when looking for update

--- src/main/setup/handleUpdates.ts
+++ src/main/setup/handleUpdates.ts
@@ -2,16 +2,21 @@ import { autoUpdater } from 'electron-updater'
 import { app, ipcMain } from 'electron'
 import { getMainWindow } from '../windows'
 import { eventNames } from '@shared/constants'
+import { error } from '@dev'
 
 export const handleUpdates = () => {
   if (!app.isPackaged) return
   autoUpdater.autoDownload = false
   autoUpdater.autoInstallOnAppQuit = true
 
-  void autoUpdater.checkForUpdates()
+  void autoUpdater.checkForUpdates().catch((e) => {
+    error('Could not find update', e)
+  })
 
   setInterval(() => {
-    void autoUpdater.checkForUpdates()
+    void autoUpdater.checkForUpdates().catch((e) => {
+      error('Could not find update', e)
+    })
   }, 30 * 60_000)
 
   autoUpdater.on('update-available', (result) => {
