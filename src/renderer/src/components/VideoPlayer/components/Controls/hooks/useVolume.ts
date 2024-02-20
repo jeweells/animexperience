b343cb2e5b2b089b11eb0664da8a3e16fb2c3964
jeweells <abraham.pacheco6319@gmail.com>
2024-02-21 00:49:15 +0100

    feat: volume is now saved and restored everytime to the last saved volume

--- src/renderer/src/components/VideoPlayer/components/Controls/hooks/useVolume.ts
+++ src/renderer/src/components/VideoPlayer/components/Controls/hooks/useVolume.ts
@@ -1,12 +1,27 @@
 import { useCallback, useEffect, useLayoutEffect, useState } from 'react'
 import { useControls } from './useControls'
 
+const VOLUME_KEY = 'volume.previous'
+
+const defaultVolume = 0.85
+
 export const useVolume = () => {
   const { video } = useControls()
-  const [volume, setVolume] = useState(0)
+  const [volume, setVolume] = useState(() => {
+    const value = localStorage.getItem(VOLUME_KEY)
+    if (value === null) return defaultVolume
+    const volume = Number(value)
+    if (!isNaN(volume) && isFinite(volume)) return volume
+    return defaultVolume
+  })
+
+  useEffect(() => {
+    localStorage.setItem(VOLUME_KEY, String(volume))
+  }, [volume])
+
   useLayoutEffect(() => {
-    if (video) return
-    setVolume(0)
+    if (!video) return
+    video.volume = volume
   }, [video])
 
   useEffect(() => {
